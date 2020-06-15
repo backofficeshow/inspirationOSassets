a6b273ef235360f8df2963780cacf49d23b08acf
Andrew Armstrong @backofficeshow <backofficeshow@users.noreply.github.com>
2020-06-15 12:46:15 +0100

    Add files via upload

--- startup.lua
+++ startup.lua
@@ -1,3 +1,6 @@
-ie.beeb()
-
+if ie.SDexists("startsound.mp3") == false then
+  ie.print("Downloading Startup Sound")
+  a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/win95.mp3","startsound.mp3")
+end
+ie.playmp3("startsound.mp3");
   
