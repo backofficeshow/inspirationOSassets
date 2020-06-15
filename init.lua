52debe920da5e5bea16ea81ce6e01c8b21c6fc18
Andrew Armstrong @backofficeshow <backofficeshow@users.noreply.github.com>
2020-06-15 15:01:50 +0100

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,13 @@
+ie.print("First run script")
+ie.print("Downloading Startup Sound")
+a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/win95.mp3","startsound.mp3")
+ie.print("Downloading i2C Scan app")
+a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/i2cscan.lua","i2cscan.lua")
+ie.print("Downloading dir command app")
+a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/dir.lua","dir.lua")
+ie.print("Downloading WIFI manager app")
+a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/wifi.lua","wifi.lua")
+ie.print("Downloading MP3 player app")
+a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/mp3.lua","mp3.lua")
+
+  
