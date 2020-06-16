04a50219264ea12a86f8d72b577c6430442a8363
Andrew Armstrong @backofficeshow <backofficeshow@users.noreply.github.com>
2020-06-16 11:50:07 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -8,25 +8,25 @@ end
 
 ie.print("First run script - connecting")
 a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/startup.lua","startup.lua")
-ie.print("Downloading Startup Script " .. printStatus(a))
+ie.print("Downloading Startup Script-" .. printStatus(a))
 
 a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/win95.mp3","startsound.mp3")
-ie.print("Downloading Startup Sound " .. printStatus(a))
+ie.print("Downloading Startup Sound-" .. printStatus(a))
 
 a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/i2cscan.lua","i2cscan.lua")
-ie.print("Downloading i2C Scan app " .. printStatus(a))
+ie.print("Downloading i2C Scan app-" .. printStatus(a))
 
 a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/dir.lua","dir.lua")
-ie.print("Downloading dir command app " .. printStatus(a))
+ie.print("Downloading dir command app-" .. printStatus(a))
 
 a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/wifi.lua","wifi.lua")
-ie.print("Downloading WIFI manager app " .. printStatus(a))
+ie.print("Downloading WIFI manager app-" .. printStatus(a))
 
 a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/mp3.lua","mp3.lua")
-ie.print("Downloading MP3 player app " .. printStatus(a))
+ie.print("Downloading MP3 player app-" .. printStatus(a))
 
 a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/init.lua","init.lua")
-ie.print("Downloading Init Script " .. printStatus(a))
+ie.print("Downloading Init Script-" .. printStatus(a))
 
 delay(2000)
 
