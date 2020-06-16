1eb3b17c9be33aff819ce9c55e52515f7e60c1c2
Andrew Armstrong @backofficeshow <backofficeshow@users.noreply.github.com>
2020-06-16 12:18:57 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,25 +7,25 @@ function printStatus(value)
 end
 
 ie.print("First run script - connecting")
-a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/startup.lua","startup.lua")
+a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/startup.lua","startup.lua")
 ie.print("Downloading Startup Script-" .. printStatus(a))
 
-a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/win95.mp3","startsound.mp3")
+a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/in95.mp3","startsound.mp3")
 ie.print("Downloading Startup Sound-" .. printStatus(a))
 
-a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/i2cscan.lua","i2cscan.lua")
+a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/i2cscan.lua","i2cscan.lua")
 ie.print("Downloading i2C Scan app-" .. printStatus(a))
 
-a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/dir.lua","dir.lua")
+a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/dir.lua","dir.lua")
 ie.print("Downloading dir command app-" .. printStatus(a))
 
-a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/wifi.lua","wifi.lua")
+a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/wifi.lua","wifi.lua")
 ie.print("Downloading WIFI manager app-" .. printStatus(a))
 
-a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/mp3.lua","mp3.lua")
+a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/mp3.lua","mp3.lua")
 ie.print("Downloading MP3 player app-" .. printStatus(a))
 
-a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/init.lua","init.lua")
+a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/init.lua","init.lua")
 ie.print("Downloading Init Script-" .. printStatus(a))
 
 delay(2000)
