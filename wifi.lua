b00bbf638f4939de1b4c5e40e86a946a30cc98ea
Andrew Armstrong @backofficeshow <backofficeshow@users.noreply.github.com>
2020-06-16 11:11:20 +0100

    Create wifi.lua

---
+++ wifi.lua
@@ -0,0 +1,8 @@
+a,b = ie.WIFIscan()
+
+for i,v in ipairs(a) do
+  print(i .. " " .. v.ssid .. " " .. v.rssi .. " " .. v.channel .. " " .. v.enc .. " " .. v.mac)
+  if i % 5 == 0 then
+    ie.inkey(true)
+  end 
+end;
