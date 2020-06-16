7e5f4aff0939d0b772cf9b271d536b95888b6f38
Andrew Armstrong @backofficeshow <backofficeshow@users.noreply.github.com>
2020-06-16 12:15:41 +0100

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,6 +1,6 @@
-if ie.SDexists("startsound.mp3") == false then
-  ie.print("Downloading Startup Sound")
-  a,b = ie.WIFIget("https://github.com/backofficeshow/inspirationOSassets/raw/master/win95.mp3","startsound.mp3")
+if ie.SDexists("startsound.mp3") == true then
+  ie.playmp3("startsound.mp3");
 end
-ie.playmp3("startsound.mp3");
+ie.print("Ready to go!")
+
   
