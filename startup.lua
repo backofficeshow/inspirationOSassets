811519b68bfb6cf9b66eadfb63d0d416ce827bd4
Andrew Armstrong @backofficeshow <backofficeshow@users.noreply.github.com>
2020-06-16 12:48:17 +0100

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,6 +1,4 @@
---if ie.SDexists("startsound.mp3") then
---ie.playmp3("startsound.mp3")
---end
+if ie.SDexists("startsound.mp3") then
+  ie.playmp3("startsound.mp3")
+end
 ie.print("Ready to go!")
-
-  
