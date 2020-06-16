0c09d8a17e083e2ed24bc6f04b5f193c03625fdf
Andrew Armstrong @backofficeshow <backofficeshow@users.noreply.github.com>
2020-06-16 13:13:10 +0100

    Update wifi.lua

--- wifi.lua
+++ wifi.lua
@@ -2,11 +2,11 @@ a,b = ie.WIFIscan()
 page = 0
 
 for i,v in ipairs(a) do
-  page = page + 1
   ie.print(i .. " " .. v.ssid .. " " .. v.rssi .. " " .. v.channel .. " " .. v.enc .. " " .. v.mac)
   if #a > 5 then
-    ie.print("Page " .. page .. " of " .. (#a / 4))
     if i % 4 == 0 then
+      page = page + 1
+      ie.print("Page " .. page .. " of " .. (#a / 4))
       ie.inkey(true)
     end 
   end
