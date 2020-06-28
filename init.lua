a362a1e92610a24987fc736eb61fc399dc1a9bcb
Andy Beer <sad_ken@hotmail.co.uk>
2020-06-28 20:31:22 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,30 +6,51 @@ function printStatus(value)
   end
 end
 
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+files = {
+	{"https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/startup.lua","startup.lua"},
+	{"https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/win95.mp3","startsound.mp3"},
+	{"https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/i2cscan.lua","i2cscan.lua"},
+	{"https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/dir.lua","dir.lua"},
+	{"https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/wifi.lua","wifi.lua"},
+	{"https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/mp3.lua","mp3.lua"},
+	{"https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/init.lua","init.lua"}}
+	
+ie.clear()
 ie.print("First run script - connecting")
-a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/startup.lua","startup.lua")
-ie.print("Downloading Startup Script-" .. printStatus(a))
+starttime = millis()
+while connected ~= true do
+	connected = ie.WIFIconnected()
+	if millis() - starttime > 5000 then 
+		ie.print("Connection Timeout")
+		goto END
+	end
+end
 
-a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/win95.mp3","startsound.mp3")
-ie.print("Downloading Startup Sound-" .. printStatus(a))
-
-a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/i2cscan.lua","i2cscan.lua")
-ie.print("Downloading i2C Scan app-" .. printStatus(a))
-
-a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/dir.lua","dir.lua")
-ie.print("Downloading dir command app-" .. printStatus(a))
-
-a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/wifi.lua","wifi.lua")
-ie.print("Downloading WIFI manager app-" .. printStatus(a))
-
-a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/mp3.lua","mp3.lua")
-ie.print("Downloading MP3 player app-" .. printStatus(a))
-
-a,b = ie.WIFIget("https://raw.githubusercontent.com/backofficeshow/inspirationOSassets/master/init.lua","init.lua")
-ie.print("Downloading Init Script-" .. printStatus(a))
-
-delay(2000)
-
-ie.reset()
-
-  
+ie.clear()
+ie.drawstring(10,10,"Downloading " ..files[1][2],1)
+ie.progressbar(10,25,108,5,0)
+ie.flip()
+count = 0
+total = tablelength(files)
+for l,k in pairs(files) do 
+	ie.clear(true)
+	a,b = ie.WIFIget(k[1],k[2])
+	ie.drawstring(10,10,"Downloading " ..k[2],1)
+	ie.drawstring(10,18,"Status - " .. printStatus(a),1)
+	count = count + 1
+	ie.progressbar(10,25,108,5,(count/total) *100)
+	ie.flip()
+end
+delay(500)
+ie.clear(true)
+ie.drawstring(10,10,"Downloading Complete",1)
+ie.drawstring(10,25,"Press Any Key to Exit",1)
+ie.flip()
+ie.inkey(true)
+ie.clear()
